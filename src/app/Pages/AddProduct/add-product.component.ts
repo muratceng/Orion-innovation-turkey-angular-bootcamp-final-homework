eb4f493e08e3454dce4c8174585607cfae31e219
Murat Çalışkan <muratcengcaliskan@gmail.com>
2022-04-07 22:01:39 +0300

    addproduct page edited

--- src/app/Pages/AddProduct/add-product.component.ts
+++ src/app/Pages/AddProduct/add-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { faAdd } from '@fortawesome/free-solid-svg-icons';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { faAdd, faRemove } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-product',
@@ -10,9 +10,9 @@ import { faAdd } from '@fortawesome/free-solid-svg-icons';
 export class AddProductComponent implements OnInit {
 
   addProductForm!:FormGroup;
-  maincategory:String[]=['Choose a main category','Electronic','HomeandFurniture','Cosmetic','Sport','Clothes'];
-  images:String[]=[''];
+  maincategory:String[]=['Electronic','HomeandFurniture','Cosmetic','Sport','Clothes'];
   faAdd = faAdd;
+  faDelete = faRemove;
 
   constructor(private formBuilder:FormBuilder) { }
 
@@ -25,30 +25,54 @@ export class AddProductComponent implements OnInit {
         title: new FormControl(null, [Validators.required]),
         shortdescription: new FormControl(null, [Validators.required]),
         description: new FormControl(null, [Validators.required]),
-        images: this.formBuilder.array(
-          this.images.map((x)=>this.formBuilder.control(x))
-        ),
+        images: this.formBuilder.array([this.createImageFormGroup()] ),
         price: new FormControl(null, [Validators.required]),
         maincategory: new FormControl(null, [Validators.required]),
         category: new FormControl(null, [Validators.required]),
     });
-}
+  }
+
+  createImageFormGroup(): FormGroup{
+    return new FormGroup({
+      'image': new FormControl(''),
+    })
+  }
 
   onSubmit(){
-    console.log('tıklandı')
-    this.images=this.addProductForm.get('images')?.value;
-    console.log(this.images);
+    let imageArray=[];
+    const formArray = this.addProductForm.get('images') as FormArray;
+    const images=formArray.controls['0'].value;
+
+    for(let i=0;i<formArray.length;i++){
+      imageArray.push(formArray.controls[i].get('image')?.value);
+    }
+
+    const product ={
+      title:this.addProductForm.get('title')?.value,
+      shortdescription:this.addProductForm.get('shortdescription')?.value,
+      description:this.addProductForm.get('description')?.value,
+      images:imageArray,
+      price:this.addProductForm.get('price')?.value,
+      maincategory:this.addProductForm.get('maincategory')?.value,
+      category:this.addProductForm.get('category')?.value
+    }
+    console.log(product);
+    this.addProductForm.reset();
   }
 
   addImages(){
-    this.images.push('');
-    this.createForm();
+    const images = this.addProductForm.get('images') as FormArray
+    images.push(this.createImageFormGroup())
   }
 
   removeImages(){
-    if(this.images.length>1){
-      this.images.pop();
-      this.createForm();
-    }
+    const images = this.addProductForm.get('images') as FormArray
+      if (images.length > 1) {
+        images.removeAt(images.length-1)
+      } 
+  }
+
+  getControls(){
+    return (this.addProductForm.get('images') as FormArray).controls;
   }
 }
